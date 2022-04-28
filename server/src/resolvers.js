f6c6dfa8e74487bad2df879d336d47d8e4aba1a2
Anthony Barnes <abarnes204@aol.com>
2022-04-28 00:00:10 -0400

    remove mocks and import resolvers into index.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,12 +2,14 @@ const resolvers = {
   Query: {
     // get all tracks, will be used to populate the homepage grid of our web client
         tracksForHome: (_, __, {dataSources}) => {
-        return dataSources.trackAPI.getTracksForHome();
+            return dataSources.trackAPI.getTracksForHome();
         }
     },
 
     Track: {
-        author: (parent, _, {dataSources}) => {}
+        author: ({authorId}, _, {dataSources}) => {
+            return dataSources.trackAPI.getAuthor(authorId);
+        }
       }
 };
 
