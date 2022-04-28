4b271576d21a37f1745ec123e3b68e02c9bd5d9a
Anthony Barnes <abarnes204@aol.com>
2022-04-28 00:04:03 -0400

    add resolvers and dataSources to ApolloServer instance - index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,18 @@
 const resolvers = require('./resolvers');
+const TrackAPI = require('./datasources/track-api');
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
 
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      trackAPI: new TrackAPI()
+    };
+  }
 });
 
 server.listen().then(() => {
