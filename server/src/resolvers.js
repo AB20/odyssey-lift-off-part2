4c085b8dfa7cbf131ce402183ac9968820904aea
Anthony Barnes <abarnes204@aol.com>
2022-04-27 23:46:52 -0400

    update server/src/resolvers.js file

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,9 +1,14 @@
 const resolvers = {
-  // tracksForHome returns an array of Tracks that will be used to populate
-  // the homepage grid of our web client
-  tracksForHome: (_, __, {dataSources}) => {
-    return dataSources.trackAPI.getTracksForHome();
-  }
+  Query: {
+    // get all tracks, will be used to populate the homepage grid of our web client
+        tracksForHome: (_, __, {dataSources}) => {
+        return dataSources.trackAPI.getTracksForHome();
+        }
+    },
+
+    Track: {
+        author: (parent, _, {dataSources) => {}
+      }
 };
 
 module.exports = resolvers;
